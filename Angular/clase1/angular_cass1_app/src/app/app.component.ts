8346cc85b1fc4b94f93779c611faba6d53a1af88
Jose <angelexe8@hotmail.com>
2025-03-15 12:56:10 +0100

    Add Class 1 x

--- Angular/clase1/angular_cass1_app/src/app/app.component.ts
+++ Angular/clase1/angular_cass1_app/src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Hello !!!!!!!!!!!!!!!';
+  title: String = 'Hello !!!!!!!!!!!!!!!';
+  users: String[] = ['Carlos','Juan','Pedro','Ana'];
+  //users: String[] = [];
+
+  visible: boolean = false;
+
+ setVisible(): void{
+  this.visible = this.visible ? false : true;
+  console.log('Click para visualizar: '+ this.visible );
+ }
+
 }
