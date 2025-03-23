aaae51a9f95dcaf9e9c118c7503a0316dffff5bd
Jose <angelexe8@hotmail.com>
2025-03-23 19:27:23 +0100

    1.3 - ESC

--- Angular/clase3/1-f-esc-app/src/app/components/cart-app.component.ts
+++ Angular/clase3/1-f-esc-app/src/app/components/cart-app.component.ts
@@ -27,7 +27,27 @@ export class CartAppComponent implements OnInit{
   this.products = this.service.findAll();
   }
 
-  onAddCart(product: Product){
-    this.items = [... this.items , {product: {...product}, quantity: 1} ];
+  onAddCart(product: Product): void{
+    const hasItem = this.items.find(item => item.product.id === product.id);
+    if(hasItem){
+      this.items = this.items.map(item => {
+        if(item.product.id === product.id){
+          return {
+           ...item,
+           quantity: item.quantity + 1
+          }
+
+        }else{
+          return item;
+        }
+      })
+    }else{
+      this.items = [... this.items , {product: {...product}, quantity: 1} ];
+      
+    }
+    
+  }
+  onDeleteCart(id: number): void{
+   this.items = this.items.filter(item => item.product.id != id)
   }
 }
--- Angular/clase3/1-f-esc-app/src/app/components/cart/cart.component.ts
+++ Angular/clase3/1-f-esc-app/src/app/components/cart/cart.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CartItem } from '../../models/cartItem';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-cart',
@@ -10,4 +11,10 @@ import { CartItem } from '../../models/cartItem';
 })
 export class CartComponent {
   @Input() items: CartItem[] = [];
+
+  @Output() idProductEventEmitter = new EventEmitter();
+
+  onDeleteCart(id: number){
+    this.idProductEventEmitter.emit(id);
+  }
 }
